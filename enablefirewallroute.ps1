6feabf587454a738af6be74e8c0042068076e677
azuregomez <edgome@microsoft.com>
2020-11-30 16:28:40 -0600

    Update enablefirewallroute.ps1

--- enablefirewallroute.ps1
+++ enablefirewallroute.ps1
@@ -1,10 +1,11 @@
 $localpath = "{your local directory}"
-$templateparamfile = $localpath + "azuredeploy.parameters.local.json"
+$templateparamfile = $localpath + "azuredeploy.parameters.json"
 $params = get-content $templateparamfile | ConvertFrom-Json
 $prefix = $params.parameters.resourcenameprefix.value
 $rgname = $prefix + "-rg"
 $pepname =  $prefix+ "-sql-pep"
 $routevnet = $prefix + "-spoke-vnet"
+$swiftvnet = "appservice-outbound-subnet"
 # get sql private endpoint
 $pe = get-azprivateendpoint -resourcegroupname $rgname -name $pepname
 # get the NIC
@@ -22,7 +23,7 @@ $rg = get-azresourcegroup -name $rgname
 $rtable = New-AzRouteTable -Name "allowapp2sql" -ResourceGroupName $rgname -Location $rg.location -Route $route
 # assign route table to subnet
 $vnet = get-azvirtualnetwork -resourcegroupname $rgname -name $routevnet
-$sqlsubnet = $vnet.subnets | where-object {$_.Name -like "*sql*"}
-set-azvirtualnetworksubnetconfig -name $sqlsubnet.name -virtualnetwork $vnet -addressprefix $sqlsubnet.addressprefix -routetable $rtable
+$appsubnet = $vnet.subnets | where-object {$_.Name -eq $swiftvnet}
+$appsubnet.RouteTable = $rtable
 $vnet | set-azvirtualnetwork
 write-host "UDR Enabled"
